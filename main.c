ec498f1f9b8825efdca4118af410f44626d99df3
Malak Wagieh <71954659+Malak252@users.noreply.github.com>
2022-01-07 00:53:35 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void Freaking_function(int x[126],bool result);
+
+int main()
+{
+
+
+    printf("It's not my fault");
+    return 0;
+}
+
+void Freaking_function( int x[126];)
+{
+    int Data;
+
+
+    for(i=0;i<126;i++)
+    {
+        if(x[i]==Data)
+        {
+            printf("nehahaha data found");
+        }
+        else
+        {
+            printf("don't cry we can't find it")
+        }
+    }
+}
