7f95228925fa85d84c8155fa187e3f55fe216b3a
Mohamed Ashraf <68484397+mohamedashraf-eng@users.noreply.github.com>
2022-01-07 00:35:46 +0200

    Add files via upload

---
+++ LinearSearch_Algorithm.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <unistd.h>
+#include <windows.h>
+#include <stdbool.h>
+#include <string.h>
+//=====================================
+
+// IMPORTANT KEYBOARD KEYS(ASCII):
+// BACKSPACE:
+#define BKSP 8
+// ENTER:
+#define ENTER 13
+// ESCAPE:
+#define ESC 27
+// SPACE:
+#define SPACE 32
+// TAB:
+#define TAB 9
+//=====================================
+
+#define MAX_DATA 100
+
+int main(void)
+{
+  system("COLOR 0D");
+
+  uint16_t DATA_BASE[MAX_DATA] = {0};
+  uint16_t SEARCH_SAMPLE = 0;
+
+
+
+
+  printf("\n");
+  return 0;
+}
