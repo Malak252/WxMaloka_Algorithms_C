cf7693d97c2abdbda61a350450cf85cfc4423ee5
Mohamed Ashraf <68484397+mohamedashraf-eng@users.noreply.github.com>
2022-01-07 01:13:59 +0200

    Update LinearSearch_Algorithm.c

--- LinearSearch_Algorithm.c
+++ LinearSearch_Algorithm.c
@@ -22,8 +22,9 @@
 //=====================================
 void ARR_DATA_IN(uint16_t *);
 void ARR_DATA_OUT(uint16_t *);
+bool LSA(uint16_t *, uint16_t );
 
-#define MAX_DATA 100
+#define MAX_DATA 255
 
 int main(void)
 {
@@ -35,27 +36,74 @@ int main(void)
   // Input random data for testing purposes
   ARR_DATA_IN(DATA_BASE);
 
-  // TESTING PURPOSE:
+  //TESTING PURPOSE:
   //printf("\n DATA STORED: \n");
   //ARR_DATA_OUT(DATA_BASE);
+
+  printf("\n \nEnter number to search: ");
+  scanf("%d", (uint32_t *) &SEARCH_SAMPLE);
+
+  printf("\n\n");
+
+  printf("\n Searching...\n");
+  if(LSA(DATA_BASE, SEARCH_SAMPLE))
+  {
+    printf("\n Data found successfully!\n");
+  }
+  else
+  {
+    printf("\n Data cannot be found!\n");
+  }
+
 
   printf("\n");
   return 0;
 }
+
+
 // Function to input random numbers using rand() function:
 void ARR_DATA_IN(uint16_t *ptr)
 {
-  for(uint8_t i = 0; (i < MAX_DATA); i++)
+  for(uint16_t i = 0; (i < MAX_DATA); i++)
   {
-    ptr[i] = rand()%150;
+    ptr[i] = rand()%255;
   }
 }
 
 // Function to output the data stored.
 void ARR_DATA_OUT(uint16_t *ptr)
 {
-  for(uint8_t i = 0; (i < MAX_DATA); i++)
+  for(uint16_t i = 0; (i < MAX_DATA); i++)
   {
     printf("%d ", ptr[i]);
+  }
+}
+
+bool LSA(uint16_t *ptr, uint16_t sample)
+{
+  uint8_t state = 0;
+
+  for(uint8_t i = 0; (i < MAX_DATA); i++)
+  {
+    if(sample == ptr[i])
+    {
+      printf("\n (%d) Found at index (%d)! \n", sample, i);
+      state = 0;
+      break;
+    }
+    else if(i == (MAX_DATA-1))
+    {
+      state = 1;
+      break;
+    }
+  }
+
+  if(state == 0)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
   }
 }
